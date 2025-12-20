f0c162b45fcf6841cadefc95dd214ead719cde18
v1kstrand <david.vikstrand@example.com>
2025-12-20 14:45:35 +0100

    f

--- csiro/config.py
+++ csiro/config.py
@@ -12,7 +12,7 @@ IDX_COLS: list[str] = [
     "Species",
     "Pre_GSHH_NDVI",
     "Height_Ave_cm",
-])
+]
 
 IMAGENET_MEAN: tuple[float, float, float] = (0.485, 0.456, 0.406)
 IMAGENET_STD: tuple[float, float, float] = (0.229, 0.224, 0.225)
