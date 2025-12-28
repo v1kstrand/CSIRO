00d52a2176b61c535cb75ec41e42ad5640e86a30
v1kstrand <david.vikstrand@example.com>
2025-12-28 12:48:12 +0100

    f

--- csiro/scripts/train_cv_func.py
+++ csiro/scripts/train_cv_func.py
@@ -81,7 +81,7 @@ def train_cv(
     for sweep in sweeps or SWEEPS:
         kwargs = dict(base_kwargs)
         kwargs.update({k: v for k, v in sweep.items()})
-        kwargs["config_name"] = str(sweep)
+        kwargs["config_name"] = "".join(c for c in str(sweep) if c.isalnum() or c in "_-:")
 
         result = run_groupkfold_cv(return_details=True, **kwargs)
         outputs.append(
